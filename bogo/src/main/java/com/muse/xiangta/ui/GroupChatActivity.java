d3a08970b23dfc485f255b89186059ccbd2b35a3
316895007@qq.com <Lu3168qq95007>
2021-04-01 18:02:49 +0800

    修改返回键大小

--- bogo/src/main/java/com/muse/xiangta/ui/GroupChatActivity.java
+++ bogo/src/main/java/com/muse/xiangta/ui/GroupChatActivity.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -134,6 +135,17 @@ public class GroupChatActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+    @OnClick(R.id.iv_back)
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+        }
     }
 
     @Override
