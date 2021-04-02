00e4ae3ecbb81716916ace45bca1b77f2717a03d
316895007@qq.com <Lu3168qq95007>
2021-04-02 14:09:22 +0800

    增加动态发布入口

--- bogo/src/main/java/com/muse/xiangta/ui/DynamicActivity.java
+++ bogo/src/main/java/com/muse/xiangta/ui/DynamicActivity.java
@@ -1,10 +1,14 @@
 package com.muse.xiangta.ui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 
 import com.muse.xiangta.R;
 import com.muse.xiangta.base.BaseActivity;
 import com.muse.xiangta.fragment.DynamicMyFragment;
+
+import butterknife.OnClick;
 
 public class DynamicActivity extends BaseActivity {
 
@@ -27,6 +31,19 @@ public class DynamicActivity extends BaseActivity {
     @Override
     protected void initSet() {
 
+    }
+
+    @OnClick(R.id.tv_fabu)
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.tv_fabu:
+                Intent intent = new Intent(DynamicActivity.this, PushDynamicActivity.class);
+                intent.putExtra("type", 2);
+                startActivity(intent);
+                break;
+        }
     }
 
     @Override
